b5c9a61a4baced40195d74d28d9490d2bc3a956b
white_sheep <white_sheep@ihteam.net>
2010-06-01 22:12:22 +0200

    perfezionato popolamento db

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `seq 2`
+for i in `seq 400`
 do
 	A="__db_instance = new Exploit(\"php$i\",\"testbb\",\"$i.0\",\"Sql Injection$i\",\"b99b6621743321cd339eIi740b36$i\",\"TEST!$i\");"
 	echo $A > ./db/exploit/test$i.db
